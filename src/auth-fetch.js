9f4967091b82f489cb53dd4b572d806089e063fc
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-17 13:12:25 -0800

    Remove base URL processing from auth-fetch.js

--- src/auth-fetch.js
+++ src/auth-fetch.js
@@ -1,11 +1,10 @@
 const fetch = require('node-fetch');
 
-exports.createAuthFetch = (baseUrl, apiKey) => async (url, options = {}) => {
+exports.createAuthFetch = apiKey => async (url, options = {}) => {
     options.headers = {
         'x-api-key': apiKey,
         'Content-Type': 'application/vnd.api+json',
         ...options.headers,
     };
-    const url = new URL(path, baseUrl);
-    return fetch(url.href, options);
+    return fetch(url, options);
 };
