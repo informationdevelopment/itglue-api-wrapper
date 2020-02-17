a9f51650b3cdb9fc3f4b02cb14b67149db572823
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-17 11:33:15 -0800

    Add url.js

---
+++ src/url.js
@@ -0,0 +1,16 @@
+// Flatten an object by wrapping nested keys with a scope.
+exports.getURLSearchParams = function(params) {
+    function flattenObjectEntries(entries, scope = []) {
+            return entries.reduce((acc, [key, value]) => {
+                if (typeof value === 'object' && value !== null) {
+                    return [...acc, ...flattenObjectEntries(Object.entries(value), [key, ...scope])];
+                }
+                else {
+                    const qualifiedKey = scope.reduce((acc, val) => `${val}[${acc}]`, key);
+                    return [...acc, [qualifiedKey, value]];
+                }
+            }, [])
+    }
+
+    return new URLSearchParams(flattenObjectEntries(Object.entries(params)));
+};
