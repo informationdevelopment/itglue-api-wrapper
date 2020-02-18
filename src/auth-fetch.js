d8a2f2e7c340b9461da7a23b29e1fb08a1893e02
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-18 10:41:43 -0800

    Refactor auth-fetch.js

--- src/auth-fetch.js
+++ src/auth-fetch.js
@@ -1,10 +1,13 @@
 const fetch = require('node-fetch');
 
-exports.createAuthFetch = apiKey => async (url, options = {}) => {
-    options.headers = {
-        'x-api-key': apiKey,
-        'Content-Type': 'application/vnd.api+json',
-        ...options.headers,
+exports.createAuthFetch = apiKey => async (url, method = 'GET', body) => {
+    options = {
+        method,
+        body,
+        headers: {
+            'x-api-key': apiKey,
+            'Content-Type': 'application/vnd.api+json',
+        },
     };
     return fetch(url, options);
 };
