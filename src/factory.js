114cde43adf72b113f2544eae10f2854f4c1e4fb
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-18 14:06:32 -0800

    Add toArray() function to indexers

--- src/factory.js
+++ src/factory.js
@@ -3,24 +3,38 @@ const { getURLSearchParams } = require('./url');
 
 exports.createFactory = (apiKey, baseUrl) => {
     const fetch = createAuthFetch(apiKey);
+
     return {
-        createIndex: endpoint => async function * (params = {}, pageSize = 500) {
-            const url = new URL(endpoint, baseUrl);
-            const urlSearchParams = getURLSearchParams(params);
-            urlSearchParams.set('page[size]', pageSize);
-            url.search = `?${urlSearchParams.toString()}`;
-            let link = url.href;
-            do {
-                const res = await fetch(link);
-                if (!res.ok) {
-                    throw new Error(`IT Glue API failure: HTTP ${res.status} ${res.statusText}`);
+        createIndex: endpoint => (params = {}, pageSize = 100) => ({
+            async * [Symbol.asyncIterator]() {
+                const url = new URL(endpoint, baseUrl);
+                const urlSearchParams = getURLSearchParams(params);
+                urlSearchParams.set('page[size]', pageSize);
+                url.search = `?${urlSearchParams.toString()}`;
+                let link = url.href;
+                do {
+                    const res = await fetch(link);
+                    if (!res.ok) {
+                        throw new Error(`IT Glue API failure: HTTP ${res.status} ${res.statusText}`);
+                    }
+                    const json = await res.json();
+                    for (let datum of json.data) {
+                        yield { ...datum, meta: json.meta };
+                    }
+                    link = json.links && json.links.next;
+                } while (link);
+            },
+
+            async toArray(count = Infinity) {
+                const result = [];
+                const iterator = this[Symbol.asyncIterator]();
+                while (result.length < count) {
+                    const { value, done } = await iterator.next();
+                    if (done) break;
+                    result.push(value);
                 }
-                const json = await res.json();
-                for (let datum of json.data) {
-                    yield { ...datum, meta: json.meta };
-                }
-                link = json.links.next;
-            } while (link);
-        },
+                return result;
+            },
+        }),
     };
 };
