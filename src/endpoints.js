701bdbf21a175ef5136d65bbd7c89492ef1982bc
David Haymond <35580802+davidhaymond@users.noreply.github.com>
2020-02-18 15:20:37 -0800

    Add contact and configuration endpoints

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,14 @@
 module.exports.buildEndpoints = factory => ({
+    configurations: {
+        index: factory.createIndex('/configurations'),
+        show: factory.createShow('/configurations'),
+    },
+
+    contacts: {
+        index: factory.createIndex('/contacts'),
+        show: factory.createShow('/contacts'),
+    },
+
     organizations: {
         index: factory.createIndex('/organizations'),
         show: factory.createShow('/organizations'),
